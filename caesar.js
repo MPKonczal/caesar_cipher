b569367189d31937a572f957ab1e0d2fdeed1c07
Mateusz Kończal <mateuszkonczal96@gmail.com>
2021-02-21 22:27:59 +0100

    Added caesar function and auxiliary functions

---
+++ caesar.js
@@ -0,0 +1,28 @@
+function validate(input) {
+  if (typeof input !== "string") {
+    throw new Error("The parameter is not of type string.");
+  } else if (input === "") {
+    throw new Error("The string is empty.");
+  } else if (regexWithoutLettersAndNumbers.test(input)) {
+    throw new Error("The string can only contain letters and numbers.");
+  }
+}
+
+const calculateNewIndex = (index) => (index + SHIFT) % ALPHABET.length;
+
+function replaceChar(char) {
+  let newChar = char.toLowerCase();
+  if (ALPHABET.includes(newChar)) {
+    const index = ALPHABET.indexOf(newChar);
+    newChar = ALPHABET[calculateNewIndex(index)];
+  }
+
+  return upperCaseRegex.test(char) ? newChar.toUpperCase() : newChar;
+}
+
+function caesar(text) {
+  validate(text);
+  const result = [...text].map(replaceChar);
+
+  return result.join("");
+}
